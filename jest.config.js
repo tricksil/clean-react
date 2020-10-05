c8a24d016ecc364ba073acfd7e60e703ace68b7a
Patrick Silva <patrickeeep12@gmail.com>
2020-10-05 10:23:33 -0300

    fix: updating wrong word

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageForm: ['<rootDir>/src/**/*.{ts, tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts, tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\ts$': 'tsjest'
+    '.+\\.(ts|tsx)$': 'ts-jest'
   }
 }
