b78c6316645237338985c079baf67e512a526d37
Patrick Silva <patrickeeep12@gmail.com>
2020-10-26 07:51:35 -0300

    test: ensure Login navigates to signup correctly

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import faker from 'faker';
 import 'jest-localstorage-mock';
 
@@ -22,12 +24,15 @@ type SutParams = {
   validationError: string;
 };
 
+const history = createMemoryHistory();
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
   const authenticationSpy = new AuthenticationSpy();
   validationStub.errorMessage = params?.validationError;
   const sut = render(
-    <Login validation={validationStub} authentication={authenticationSpy} />
+    <Router history={history}>
+      <Login validation={validationStub} authentication={authenticationSpy} />
+    </Router>
   );
   return {
     sut,
@@ -182,5 +187,13 @@ describe('Login Component', () => {
       'accessToken',
       authenticationSpy.account.accessToken
     );
+  });
+
+  test('Should go to signup page', async () => {
+    const { sut } = makeSut();
+    const register = sut.getByTestId('signup');
+    fireEvent.click(register);
+    expect(history.length).toBe(2);
+    expect(history.location.pathname).toBe('/signup');
   });
 });
