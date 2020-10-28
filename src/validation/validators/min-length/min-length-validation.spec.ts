2851afc236a1225eec50cc2809f1cf73c63ca6f4
Patrick Silva <patrickeeep12@gmail.com>
2020-10-28 08:36:13 -0300

    test: ensure MinLengthValidation returns falsy if value is not invalid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,4 +7,10 @@ describe('MinLengthValidation', () => {
     const error = sut.validate('123');
     expect(error).toEqual(new InvalidFieldError());
   });
+
+  test('Should return falsy if value is not invalid', () => {
+    const sut = new MinLengthValidation('field', 5);
+    const error = sut.validate('12345');
+    expect(error).toBeFalsy();
+  });
 });
